b06cb9895cd4edf7ae10e0245895e316cd5e2d38
olgarme <77417318+olgarpsb@users.noreply.github.com>
2024-05-01 15:43:50 -0500

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,15 +1,22 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+//importing expense and budget from context and getting the remaining value using subtraction.
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 const Remaining = () => {
-    const { expenses, budget } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total = total + item.cost);
-    }, 0);
-    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
-    return (
-        <div className={`alert ${alertType}`}>
-            <span>Remaining: £{budget - totalExpenses}</span>
-        </div>
-    );
+  const { expenses, budget, currency } = useContext(AppContext);
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total = total + item.cost);
+  }, 0);
+
+  const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
+
+  return (
+    <div className={`alert ${alertType}`}>
+      <span>
+        Remaining: {currency}
+        {budget - totalExpenses}
+      </span>
+    </div>
+  );
 };
 export default Remaining;
